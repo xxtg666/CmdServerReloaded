45bf8122bb7466ccab11edce4519416a48ea6412
xxtg666 <xxtg666@gmail.com>
2022-12-30 23:16:36 +0800

    Pre-release v1.0.2

--- csr-client.py
+++ csr-client.py
@@ -1,20 +1,25 @@
-# TO-DO:自动重连
 import os
 import json
 import socket
-import urllib
+import urllib.request
 from threading import Thread
 import subprocess
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import time
 import base64
 import getpass
+import tkinter.messagebox
+import traceback
+
+
 def base64encode(string):
     return base64.b64encode(string.encode()).decode()
+
+
 def base64decode(string):
     return base64.b64decode(string.encode()).decode()
 
-s = socket.socket()
+
 start_time = time.time()  # 起始运行时间
 
 # 初始化服务器连接配置
@@ -29,13 +34,14 @@ else:
         "server-address": "127.0.0.1",
         "server-port": "8631",
         "fallback-server-port": "8632",
-        "startup-message-time": "0"
+        "startup-message-time": "0",
     }
     with open("config.json", "w") as config_file:
         json.dump(config, config_file)
 if config["startup-message-time"] != "0":
     os.system(
-        f'msg %username% /time:{config["startup-message-time"]} "CmdServerReloaded | ClientName: {config["client-name"]} | LocalHTTP: {config["local-port"]} | ConnectToServer: {config["server-address"]}')
+        f'msg %username% /time:{config["startup-message-time"]} "CmdServerReloaded | ClientName: {config["client-name"]} | LocalHTTP: {config["local-port"]} | ConnectToServer: {config["server-address"]}'
+    )
 
 
 # 内置状态显示HTTP服务器
@@ -47,19 +53,22 @@ class Request(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        buf = f'''<title>CmdServerReloaded - Status</title>config.json {str(config)}
+        buf = f"""<title>CmdServerReloaded - Status</title>config.json {str(config)}
 Version - {config["version"]}
 HostName - {socket.gethostname()}
 UserName - {getpass.getuser()}
 HttpPort - {config["local-port"]}
 ConnectServer - {config["server-address"]}
 ConnectStatus - {False}
-RunningTime - {time.time() - start_time}'''.replace("\n", "<br>")
+RunningTime - {time.time() - start_time}""".replace(
+            "\n", "<br>"
+        )
         self.wfile.write(buf.encode("gb2312"))
 
 
 def start_http_server():
-    HTTPServer(('localhost', int(config["local-port"])), Request).serve_forever()
+    HTTPServer(("localhost", int(config["local-port"])), Request).serve_forever()
+
 
 Thread(target=start_http_server).start()
 
@@ -73,17 +82,21 @@ def e_python(code):
 
 
 def e_message(text):
-    os.system(f'msg %username% "{text}"')
+    tkinter.messagebox.showinfo("Message From Server", text)
 
 
 def execute(mode, line):
     if mode == "cmd":
-        return subprocess.Popen(line,
-                                shell=True,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT,
-                                encoding='gb2312'
-                                ).communicate()[0]
+        p = subprocess.Popen(
+            line,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        ).communicate()[0]
+        try:
+            return p.decode("gb2312")
+        except:
+            return p.decode("UTF-8")
     elif mode == "download":
         Thread(target=e_download, args=(line,)).start()
         return "Done"
@@ -95,21 +108,36 @@ def execute(mode, line):
         return "Done"
     elif mode == "username":
         return config["client-name"]
+
+
 def execute_and_fallbackdata(data):
     res = execute(data[0], base64decode(data[1]))
     s2 = socket.socket()
     s2.connect((config["server-address"], int(config["fallback-server-port"])))
-    s2.send(f'''{data[2]}|{base64encode(res)}'''.encode("UTF-8"))
+    s2.send(f"""{data[2]}|{base64encode(res)}""".encode("UTF-8"))
     print(base64encode(res))
     s2.close()
+
+
 def start_server_connection():
-    global s
-    s.connect((config["server-address"], int(config["server-port"])))
     while True:
-        d=s.recv(1024000).decode('UTF-8')
-        print(d)
-        data=d.split("|")
-        Thread(target=execute_and_fallbackdata,args=(data,)).start()
+        s = socket.socket()
+        try:
+            s.connect((config["server-address"], int(config["server-port"])))
+        except:
+            print(traceback.format_exc())
+            continue
+        while True:
+            try:
+                d = s.recv(1024000).decode("UTF-8")
+            except:
+                print(traceback.format_exc())
+                break
+            print(d)
+            data = d.split("|")
+            Thread(target=execute_and_fallbackdata, args=(data,)).start()
+        s.close()
+        time.sleep(5)
 
 
 if __name__ == "__main__":
